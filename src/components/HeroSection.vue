04f1c4be26aec1aa2b0b61989fc67b28c8893056
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-03 18:54:05 -0300

    feat: make HeroSection component

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div
+      id="user-introduction"
+      class="d-flex bg-light"
+    >
+      <div
+        id="user-photo"
+        class="col-md-6 p-3 bg-transparent"
+      >
+        <img
+          src="../assets/user.photo.svg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div
+        id="user-text"
+        class="col-md-6 p-3 bg-transparent"
+      >
+        <p class="fs-1 fw-light">Giovanni Vasconcelos</p>
+        <p class="fs-5 fw-light">
+          Full-Stack Web Developer PHP | JS | Laravel | Vue.js | Bootstrap | Tailwind CSS | MySQL
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
