d99c0a59fd859ab7ef8861f7641969b69aea4164
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-03 11:18:14 -0300

    chore: edit ESLint and Prettier files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,16 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    // '@vue/eslint-config-prettier/skip-formatting'
+    'prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+    'vue/require-default-prop': 'off',
+  },
+};
