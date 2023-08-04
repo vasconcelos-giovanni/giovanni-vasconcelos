026cd119967811c50ad5e20a694ac738d6fc5074
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-04 14:24:12 -0300

    feat: import main.scss

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import { i18n } from './i18n';
+import './styles/main.scss';
 
 const app = createApp(App);
 app.use(router);
