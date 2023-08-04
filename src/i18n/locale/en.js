7e7d25e1790357e2b11e78ba2c06a53a9e8f9704
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-04 17:06:02 -0300

    feat: add aboutUserSection to homeView

--- src/i18n/locale/en.js
+++ src/i18n/locale/en.js
@@ -12,8 +12,15 @@ export const en = {
   },
   views: {
     homeView: {
-      heroSection: {
+      heroArticle: {
         userDescription: 'Full-Stack Web Developer ',
+      },
+      aboutUserSection: {
+        heading: 'About me',
+        content: [
+          'With a passion for learning and a keen focus on practical and maintainable solutions, I thrive on tackling challenges head-on while embracing effective communication. My journey in web development is guided by a commitment to continuous growth and delivering high-quality results.',
+          'Something more',
+        ],
       },
     },
   },
