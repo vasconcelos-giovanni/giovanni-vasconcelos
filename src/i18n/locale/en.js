76f44c66a41723882acf59cded89b87b289b54f6
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-04 13:50:42 -0300

    feat: make en.js lang file

---
+++ src/i18n/locale/en.js
@@ -0,0 +1,22 @@
+import { toRaw } from 'vue';
+
+export const en = {
+  layouts: {
+    mainLayout: {
+      header: {
+        navbar: {
+          projects: 'Projects',
+          aboutWebsite: 'About Website',
+        },
+      },
+      footer: {},
+    },
+  },
+  views: {
+    homeView: {
+      heroSection: {
+        userDescription: 'Full-Stack Web Developer ',
+      },
+    },
+  },
+};
