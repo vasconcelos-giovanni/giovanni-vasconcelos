a58f9f2233245223fdca8da97d25c9b71d788b14
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-04 17:06:57 -0300

    feat: make AboutUserSection for HomeView

---
+++ src/components/AboutUserSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <SectionTemplate>
+    <h2>{{ $t('views.homeView.aboutUserSection.heading') }}</h2>
+    <p>{{ $t('views.homeView.aboutUserSection.content[0]') }}</p>
+
+    <!-- <p
+      v-for="paragraph of $t('views.homeView.aboutUserSection.content')"
+      :key="paragraph"
+    >
+      {{ paragraph }}
+    </p> -->
+
+    <!-- <p>{{ state.aboutUserSection.content[0] }}</p> -->
+  </SectionTemplate>
+</template>
+
+<script setup>
+import { SectionTemplate } from '.';
+</script>
