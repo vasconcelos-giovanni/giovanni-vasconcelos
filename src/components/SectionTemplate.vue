293ce78b322aae24ad122a7cd2b478c36cf023bf
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-04 16:06:25 -0300

    style: make <p> justified

---
+++ src/components/SectionTemplate.vue
@@ -0,0 +1,11 @@
+<template>
+  <section v-bind="$attrs">
+    <slot />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+p {
+  text-align: justify;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import HeroSection from './HeroSection.vue';
 import NavBar from './NavBar.vue';
 import NavLink from './NavLink.vue';
+import SectionTemplate from './SectionTemplate.vue';
 
-export { HeroSection, NavBar, NavLink };
+export { HeroSection, NavBar, NavLink, SectionTemplate };
