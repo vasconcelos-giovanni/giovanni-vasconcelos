1f08cb24b20f01fc909e014e057f5ae2b531260e
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 12:39:15 -0300

    feat: update with new components

--- src/components/index.js
+++ src/components/index.js
@@ -6,10 +6,13 @@ import EducationSection from './EducationSection.vue';
 import FrontendSection from './FrontendSection.vue';
 import HeroArticle from './HeroArticle.vue';
 import LanguagesSection from './LanguagesSection.vue';
+import MainFooter from './MainFooter.vue';
 import NavBar from './NavBar.vue';
 import NavLink from './NavLink.vue';
 import OthersSection from './OthersSection.vue';
 import ProgrammingLanguagesSection from './ProgrammingLanguagesSection.vue';
+import PortifolioItemSlide from './PortifolioItemSlide.vue';
+import PortifolioSection from './PortifolioSection.vue';
 import SectionTemplate from './SectionTemplate.vue';
 import SkillsArticle from './SkillsArticle.vue';
 import SkillImage from './SkillImage.vue';
@@ -24,10 +27,13 @@ export {
   FrontendSection,
   HeroArticle,
   LanguagesSection,
+  MainFooter,
   NavBar,
   NavLink,
   OthersSection,
   ProgrammingLanguagesSection,
+  PortifolioItemSlide,
+  PortifolioSection,
   SectionTemplate,
   SkillsArticle,
   SkillImage,
