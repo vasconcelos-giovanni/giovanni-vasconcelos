5494c308f521a1963128d7f803ac72b14f19e026
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 08:50:50 -0300

    feat: make Education components

---
+++ src/components/DegreesListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>
+    <span class="fw-bold">{{ props.degree }}</span
+    ><span class="fst-italic"> - {{ props.institution }}</span
+    ><span> - {{ props.time }}</span>
+  </p>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  degree: {
+    type: String,
+    required: true,
+  },
+  institution: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
+</script>
---
+++ src/components/EducationSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <SectionTemplate>
+    <h2>{{ $t('views.homeView.aboutUserArticle.educationSection.heading') }}</h2>
+
+    <DegreesListItem
+      :degree="$t('views.homeView.aboutUserArticle.educationSection.degreesList.li0.degree')"
+      :institution="
+        $t('views.homeView.aboutUserArticle.educationSection.degreesList.li0.institution')
+      "
+      :time="$t('views.homeView.aboutUserArticle.educationSection.degreesList.li0.time')"
+    />
+    <DegreesListItem
+      :degree="$t('views.homeView.aboutUserArticle.educationSection.degreesList.li1.degree')"
+      :institution="
+        $t('views.homeView.aboutUserArticle.educationSection.degreesList.li1.institution')
+      "
+      :time="$t('views.homeView.aboutUserArticle.educationSection.degreesList.li1.time')"
+    />
+  </SectionTemplate>
+</template>
+
+<script setup>
+import { DegreesListItem, SectionTemplate } from '.';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
