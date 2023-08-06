38c61dd839f45769f71a85c5c5eee615acdef86c
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 12:37:59 -0300

    feat: make footer of MainLayout with contact info

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <SectionTemplate>
+    <h2 class="fs-6">Find me</h2>
+    <ul class="list-unstyled">
+      <li>
+        <a href="mailto:vasconcelos.giovanni@hotmail.com">E-mail</a>
+      </li>
+      <li><a href="https://www.linkedin.com/in/vasconcelos-giovanni">LinkedIn</a></li>
+      <li><a href="https://my.indeed.com/p/giovanniv-mfy2ybq">Indeed</a></li>
+      <li><a href="https://github.com/vasconcelos-giovanni">GitHub</a></li>
+    </ul>
+  </SectionTemplate>
+</template>
+
+<script setup>
+import { SectionTemplate } from '.';
+</script>
