33a5a796a8f8bd906b172e121ebe57bb8c2e2ed7
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 08:47:49 -0300

    style: remove unecessary comments

--- src/components/AboutUserSection.vue
+++ src/components/AboutUserSection.vue
@@ -1,16 +1,7 @@
 <template>
   <SectionTemplate>
-    <h2>{{ $t('views.homeView.aboutUserSection.heading') }}</h2>
-    <p>{{ $t('views.homeView.aboutUserSection.content[0]') }}</p>
-
-    <!-- <p
-      v-for="paragraph of $t('views.homeView.aboutUserSection.content')"
-      :key="paragraph"
-    >
-      {{ paragraph }}
-    </p> -->
-
-    <!-- <p>{{ state.aboutUserSection.content[0] }}</p> -->
+    <h2>{{ $t('views.homeView.aboutUserArticle.aboutSection.heading') }}</h2>
+    <p>{{ $t('views.homeView.aboutUserArticle.aboutSection.p0') }}</p>
   </SectionTemplate>
 </template>
 
