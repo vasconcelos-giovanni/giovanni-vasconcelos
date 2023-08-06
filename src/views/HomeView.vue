51870e13763c5defbc3cb6f625e907adb28e3bdb
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 08:52:44 -0300

    feat: add new sections to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,22 @@
     <template #hero>
       <HeroArticle />
     </template>
-    <AboutUserSection />
+    <article>
+      <AboutUserSection />
+      <SkillsArticle />
+      <LanguagesSection />
+      <EducationSection />
+    </article>
   </MainLayout>
 </template>
 
 <script setup>
+import {
+  AboutUserSection,
+  EducationSection,
+  HeroArticle,
+  LanguagesSection,
+  SkillsArticle,
+} from '../components';
 import { MainLayout } from '../layouts';
-import { HeroArticle } from '../components';
-import { AboutUserSection } from '../components';
 </script>
