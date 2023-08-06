eab6494193aef81537801228a1460e7045696e41
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 14:06:11 -0300

    refactor: separate slideshow from the section to be used in other components

---
+++ src/components/PortifolioArticle.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <PortifolioSlideShow>
+      <PortifolioItemSlide
+        src="https://github.com/vasconcelos-giovanni/gio-chirps/raw/main/readme/img/screenshots/1.png"
+        :alt="$t('views.homeView.aboutUserArticle.portifolioSection.slides.slide0.alt')"
+        class="active" />
+      <PortifolioItemSlide
+        src="https://github.com/vasconcelos-giovanni/divination-crystal-ball/raw/main/src/img/screenshot.png"
+        :alt="$t('views.homeView.aboutUserArticle.portifolioSection.slides.slide0.alt')" />
+      <PortifolioItemSlide
+        src="https://github.com/vasconcelos-giovanni/books-recorder/raw/main/readme/img/screenshots/1.png"
+        :alt="$t('views.homeView.aboutUserArticle.portifolioSection.slides.slide0.alt')"
+    /></PortifolioSlideShow>
+  </article>
+</template>
+
+<script setup>
+import { PortifolioItemSlide, PortifolioSlideShow } from '.';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
