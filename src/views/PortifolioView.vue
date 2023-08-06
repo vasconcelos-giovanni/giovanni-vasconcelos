21c05a73bf866b7b27566a006c29cf4c5002cd3b
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 14:09:27 -0300

    feat: make PortifolioView

---
+++ src/views/PortifolioView.vue
@@ -0,0 +1,10 @@
+<template>
+  <MainLayout>
+    <template #hero> </template>
+  </MainLayout>
+</template>
+
+<script setup>
+import { MainLayout } from '../layouts';
+// import { PortifolioView } from '../components';
+</script>
