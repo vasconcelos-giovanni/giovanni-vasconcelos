88d531a07d7c568d01869163b761017491e15979
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 08:54:05 -0300

    feat: update /src/components/index.js with the new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,35 @@
 import AboutUserSection from './AboutUserSection.vue';
+import BackendSection from './BackendSection.vue';
+import DatabaseSection from './DatabaseSection.vue';
+import DegreesListItem from './DegreesListItem.vue';
+import EducationSection from './EducationSection.vue';
+import FrontendSection from './FrontendSection.vue';
 import HeroArticle from './HeroArticle.vue';
+import LanguagesSection from './LanguagesSection.vue';
 import NavBar from './NavBar.vue';
 import NavLink from './NavLink.vue';
+import OthersSection from './OthersSection.vue';
+import ProgrammingLanguagesSection from './ProgrammingLanguagesSection.vue';
 import SectionTemplate from './SectionTemplate.vue';
+import SkillsArticle from './SkillsArticle.vue';
+import SkillImage from './SkillImage.vue';
+import SkillsGroup from './SkillsGroup.vue';
 
-export { AboutUserSection, HeroArticle, NavBar, NavLink, SectionTemplate };
+export {
+  AboutUserSection,
+  BackendSection,
+  DatabaseSection,
+  DegreesListItem,
+  EducationSection,
+  FrontendSection,
+  HeroArticle,
+  LanguagesSection,
+  NavBar,
+  NavLink,
+  OthersSection,
+  ProgrammingLanguagesSection,
+  SectionTemplate,
+  SkillsArticle,
+  SkillImage,
+  SkillsGroup,
+};
