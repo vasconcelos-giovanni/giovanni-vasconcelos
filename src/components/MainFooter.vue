5230b2431d26f74f117cac565e898ef769e357d1
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-06 14:05:18 -0300

    style: make list horizontal

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,17 +1,13 @@
 <template>
-  <SectionTemplate>
+  <div class="d-flex flex-column align-items-center">
     <h2 class="fs-6">Find me</h2>
-    <ul class="list-unstyled">
-      <li>
+    <ul class="list-unstyled d-flex flex-wrap">
+      <li class="me-3">
         <a href="mailto:vasconcelos.giovanni@hotmail.com">E-mail</a>
       </li>
-      <li><a href="https://www.linkedin.com/in/vasconcelos-giovanni">LinkedIn</a></li>
-      <li><a href="https://my.indeed.com/p/giovanniv-mfy2ybq">Indeed</a></li>
+      <li class="me-3"><a href="https://www.linkedin.com/in/vasconcelos-giovanni">LinkedIn</a></li>
+      <li class="me-3"><a href="https://my.indeed.com/p/giovanniv-mfy2ybq">Indeed</a></li>
       <li><a href="https://github.com/vasconcelos-giovanni">GitHub</a></li>
     </ul>
-  </SectionTemplate>
+  </div>
 </template>
-
-<script setup>
-import { SectionTemplate } from '.';
-</script>
