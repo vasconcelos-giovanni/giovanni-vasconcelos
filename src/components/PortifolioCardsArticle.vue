65fd5aceaadaf5cd8cb00ec16441d3eb34a601fb
Giovanni Vasconcelos <vasconcelos.givanni@hotmail.com>
2023-08-08 22:19:26 -0300

    refactor: turn repositories cards into flush list in portifolio page

--- src/components/PortifolioCardsArticle.vue
+++ src/components/PortifolioCardsArticle.vue
@@ -1,53 +1,55 @@
 <template>
   <article>
-    <div
-      class="card col-xs-11 col-sm-8 col-lg-6 col-xl-5"
-      v-for="repo in repos"
-      :key="repo.id"
-    >
-      <div class="card-body d-inline-flex flex-column justi">
-        <h5 class="card-title text-start fw-bold">{{ repo.name }}</h5>
-        <p class="card-text">
-          {{ repo.description }}
-        </p>
-        <ul class="list-unstyled d-flex flex-wrap gap-2">
-          <li
-            class="badge fs-6 text-secondary border border-secondary rounded-pill"
-            v-for="topic of repo.topics"
-            :key="topic"
-          >
-            {{ topic }}
-          </li>
-        </ul>
-        <div class="d-flex justify-content-start flex-wrap gap-2">
-          <a
-            :href="repo.html_url"
-            target="_blank"
-            class="btn btn-outline-dark d-inline-flex align-items-center gap-1"
-            @mouseover="setHovered(true, repo.id)"
-            @mouseout="setHovered(false, repo.id)"
-          >
-            <img
-              src="../assets/github-mark-white.svg"
-              class="gh-link d-inline"
-              v-if="isHovered[repo.id]"
-            />
-            <img
-              src="../assets/github-mark.svg"
-              class="gh-link d-inline"
-              v-else
-            />
-            <span>Repository</span>
-          </a>
-          <a
-            :href="repo.homepage"
-            class="btn btn-outline-secondary"
-            v-if="repo.homepage != '' && repo.homepage != null"
-            >💿 Live demo</a
-          >
+    <ul class="list-group list-group-flush">
+      <li
+        class="list-group-item p-3"
+        v-for="repo in repos"
+        :key="repo.id"
+      >
+        <div class="d-flex flex-column gap-3">
+          <h5 class="card-title text-start fw-bold">{{ repo.name }}</h5>
+          <p class="card-text">
+            {{ repo.description }}
+          </p>
+          <ul class="list-unstyled d-flex flex-wrap gap-2">
+            <li
+              class="badge fs-6 text-secondary border border-secondary rounded-pill"
+              v-for="topic of repo.topics"
+              :key="topic"
+            >
+              {{ topic }}
+            </li>
+          </ul>
+          <div class="d-flex justify-content-start flex-wrap gap-2">
+            <a
+              :href="repo.html_url"
+              target="_blank"
+              class="btn btn-outline-dark d-inline-flex align-items-center gap-1"
+              @mouseover="setHovered(true, repo.id)"
+              @mouseout="setHovered(false, repo.id)"
+            >
+              <img
+                src="../assets/github-mark-white.svg"
+                class="gh-link d-inline"
+                v-if="isHovered[repo.id]"
+              />
+              <img
+                src="../assets/github-mark.svg"
+                class="gh-link d-inline"
+                v-else
+              />
+              <span>Repository</span>
+            </a>
+            <a
+              :href="repo.homepage"
+              class="btn btn-outline-secondary"
+              v-if="repo.homepage != '' && repo.homepage != null"
+              >💿 Live demo</a
+            >
+          </div>
         </div>
-      </div>
-    </div>
+      </li>
+    </ul>
   </article>
 </template>
 
@@ -55,6 +57,7 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
+// Repos GET
 const repos = ref([]);
 
 const getRepos = async () => {
@@ -73,6 +76,7 @@ onMounted(async () => {
   getRepos();
 });
 
+// GitHub button icon switching to dark and light
 const isHovered = ref({});
 const setHovered = (value, id) => {
   isHovered.value[id] = value;
